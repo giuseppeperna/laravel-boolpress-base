f840e078c58ba4ab1c081fc25ae92c0fed0c3465
giuseppeperna <giuseppeperna.cg@gmail.com>
2021-02-02 16:18:19 +0100

    Laravel Boolpress - factories, seeding with relationships

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Category::class, function (Faker $faker) {
     return [
-        'title'=> $faker->sentence(),
+        'title'=> $faker->word,
         'slug'=> $faker->slug
     ];
 });
